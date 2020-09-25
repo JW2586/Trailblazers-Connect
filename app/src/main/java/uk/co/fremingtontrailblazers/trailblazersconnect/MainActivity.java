2e905fe30ce322e3fa70a88b0628ded6a819343b
JW2586 <jakers.wickham@gmail.com>
2020-09-25 14:16:41 +0100

    New Logout button added

--- app/src/main/java/uk/co/fremingtontrailblazers/trailblazersconnect/MainActivity.java
+++ app/src/main/java/uk/co/fremingtontrailblazers/trailblazersconnect/MainActivity.java
@@ -4,21 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    Button mLogoutBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void logout(View view) {
-        FirebaseAuth.getInstance().signOut();                   //logs out the user when the logout button is pressed
-        startActivity(new Intent(getApplicationContext(),Login.class));
-        finish();
+        mLogoutBtn = findViewById(R.id.logoutButton);
+        mLogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLogoutBtn.setBackgroundResource(R.drawable.logout_button_pressed);
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mLogoutBtn.setBackgroundResource(R.drawable.logout_button);
+                    }
+                }, 100);
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
     }
 }
