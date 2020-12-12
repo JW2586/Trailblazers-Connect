9cc622580dd4acdb8afaea85368bdfa72a9fa348
JW2586 <jakers.wickham@gmail.com>
2020-12-12 14:26:13 +0000

    Updated UI with DatePicker & TimePicker. Title and location input added too

---
+++ app/src/main/java/uk/co/fremingtontrailblazers/trailblazersconnect/DatePickerFragment.java
@@ -0,0 +1,27 @@
+package uk.co.fremingtontrailblazers.trailblazersconnect;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment {
+    Calendar datePickerCalendar = Calendar.getInstance();
+    int year = datePickerCalendar.get(Calendar.YEAR);
+    int month = datePickerCalendar.get(Calendar.MONTH);
+    int day = datePickerCalendar.get(Calendar.DAY_OF_MONTH);
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+    }
+}
--- app/src/main/java/uk/co/fremingtontrailblazers/trailblazersconnect/createEventActivity.java
+++ app/src/main/java/uk/co/fremingtontrailblazers/trailblazersconnect/createEventActivity.java
@@ -2,8 +2,12 @@ package uk.co.fremingtontrailblazers.trailblazersconnect;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.DialogFragment;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,7 +16,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.applandeo.materialcalendarview.CalendarView;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -23,6 +30,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firestore.v1.Document;
 
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -32,12 +40,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class createEventActivity extends AppCompatActivity {
+import static android.view.View.VISIBLE;
+
+public class createEventActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+    private static final String TITLE_KEY = "Title";
     private static final String DETAILS_KEY = "Details";
+    private static final String LOCATION_KEY = "Location";
     private static final String DATE_KEY = "Date";
     private static final String GROUPS_KEY = "Groups";
+    private int eventDateYear = 0;
+    private int eventDateMonth = 0;
+    private int eventDateDay = 0;
+    private int eventHour = 0;
+    private int eventMinute = 0;
     Button mSaveEventButton;                                //creates variables for each UI element
     EditText mEventDetailsView;
+    EditText mEventTitleView;
+    EditText mEventLocationView;
     CalendarView mDatePicker;
     CheckBox mCheckBoxRed;
     CheckBox mCheckBoxOrange;
@@ -46,7 +65,10 @@ public class createEventActivity extends AppCompatActivity {
     CheckBox mCheckBoxBlue;
     CheckBox mCheckBoxIndigo;
     CheckBox mCheckBoxViolet;
-
+    Button mSelectDateButton;
+    Button mSelectTimeButton;
+    TextView mDisplayDate;
+    TextView mDisplayTime;
     private CollectionReference mColRef = FirebaseFirestore.getInstance().collection("events");     //sets the cloud firestore collection to be the "events" collection
 
     @Override
@@ -63,8 +85,29 @@ public class createEventActivity extends AppCompatActivity {
         mCheckBoxViolet = findViewById(R.id.checkBoxViolet);
         mDatePicker = findViewById(R.id.eventCalendar);
         mEventDetailsView = findViewById(R.id.eventText);
+        mEventTitleView = findViewById(R.id.eventTitle);
+        mEventLocationView = findViewById(R.id.eventLocation);
         mSaveEventButton = findViewById(R.id.saveEventButton);
+        mSelectDateButton = findViewById(R.id.selectDateButton);
+        mSelectTimeButton = findViewById(R.id.selectTimeButton);
+        mDisplayDate = findViewById(R.id.displayDate);
+        mDisplayTime = findViewById(R.id.displayTime);
+        mSelectDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
 
+            }
+        });
+
+        mSelectTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
         mSaveEventButton.setOnClickListener(new View.OnClickListener() {            //When the SaveEventButton is pressed, it plays the animation and calls the createEvent function
             @Override
             public void onClick(View v) {
@@ -84,16 +127,46 @@ public class createEventActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        this.eventDateYear = year;
+        this.eventDateMonth = month;
+        this.eventDateDay = dayOfMonth;
+        Calendar datePickerCalendar = Calendar.getInstance();
+        datePickerCalendar.set(Calendar.YEAR, year);
+        datePickerCalendar.set(Calendar.MONTH, month);
+        datePickerCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance().format(datePickerCalendar.getTime());
+        String dateToDisplay = "Selected Date: " + currentDateString;
+        mDisplayDate.setText(dateToDisplay);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        this.eventHour = hourOfDay;
+        this.eventMinute = minute;
+        if (String.valueOf(minute).length() == 1) {
+            String strMinute = "0" + minute;
+            String timeToDisplay = "Selected Time: " + hourOfDay + ":" + strMinute;
+            mDisplayTime.setText(timeToDisplay);
+        } else {
+            String timeToDisplay = "Selected Time: " + hourOfDay + ":" + minute;
+            mDisplayTime.setText(timeToDisplay);
+        }
+    }
+
     private void createEvent() {
         List<String> groupsList = new ArrayList<String>();                  //creates new array to store the list of groups for the event
+//        Calendar eventDate =  mDatePicker.getFirstSelectedDate();      //gets the data that is selected on the calendar by the user
+//        Log.i("eventDate", String.valueOf(eventDate));              //outputs the date, month and year for debugging purposes
+//        Log.i("eventDateYear", String.valueOf(eventDate.get(1)));
+//        Log.i("eventDateMonth", String.valueOf((eventDate.get(2))+1));
+//        Log.i("eventDateYear", String.valueOf(eventDate.get(5)));
+//
+//        Calendar myCalendar = new GregorianCalendar(eventDate.get(1), eventDate.get(2), eventDate.get(5));    //creates a Date data type and passes it the date, month and year of the event
 
-        Calendar eventDate =  mDatePicker.getFirstSelectedDate();      //gets the data that is selected on the calendar by the user
-        Log.i("eventDate", String.valueOf(eventDate));              //outputs the date, month and year for debugging purposes
-        Log.i("eventDateYear", String.valueOf(eventDate.get(1)));
-        Log.i("eventDateMonth", String.valueOf((eventDate.get(2))+1));
-        Log.i("eventDateYear", String.valueOf(eventDate.get(5)));
+        Calendar myCalendar = new GregorianCalendar(eventDateYear, eventDateMonth, eventDateDay, eventHour, eventMinute);    //creates a Date data type and passes it the date, month and year of the event
 
-        Calendar myCalendar = new GregorianCalendar(eventDate.get(1), eventDate.get(2), eventDate.get(5));    //creates a Date data type and passes it the date, month and year of the event
         Date finalEventDate = myCalendar.getTime();
         Log.i("final date", String.valueOf(finalEventDate));
 
@@ -134,11 +207,15 @@ public class createEventActivity extends AppCompatActivity {
             Log.i("list", groupsList.toString());
         }
 
+        String eventTitle = mEventTitleView.getText().toString().trim();    //gets the text typed in the title box and saves it as a string
         String eventDetails = mEventDetailsView.getText().toString().trim();    //gets the text typed in the details box and saves it as a string
-        Log.i("details", eventDetails);
+        String eventLocation = mEventLocationView.getText().toString().trim();    //gets the text typed in the location box and saves it as a string
+//        Log.i("details", eventDetails);
 
         Map<String, Object> eventToSave = new HashMap<String, Object>();                    //creates a hashMap that stores the event data before it is saved to cloud firestore
-        eventToSave.put(DETAILS_KEY, eventDetails);                            //adds all the required data to the hashMap
+        eventToSave.put(TITLE_KEY, eventTitle);                            //adds all the required data to the hashMap
+        eventToSave.put(DETAILS_KEY, eventDetails);
+        eventToSave.put(LOCATION_KEY, eventLocation);
         eventToSave.put(DATE_KEY, finalEventDate);
         eventToSave.put(GROUPS_KEY, groupsList);
         mColRef.add(eventToSave).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {      //saves the data to firestore and checks if the request is successful
@@ -156,4 +233,5 @@ public class createEventActivity extends AppCompatActivity {
 
 
     }
+
 }
